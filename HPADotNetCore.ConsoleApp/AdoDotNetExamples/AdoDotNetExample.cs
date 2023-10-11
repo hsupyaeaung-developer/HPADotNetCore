06bd47bd13dfcddbc9774a56b771d0c89b89c758
Hsu Pyae Aung <hsupyaeaung11219@gmail.com>
2023-10-11 21:42:17 +0630

    Add project files.

---
+++ HPADotNetCore.ConsoleApp/AdoDotNetExamples/AdoDotNetExample.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace HPADotNetCore.ConsoleApp.AdoDotNetExamples
+{
+    public class AdoDotNetExample
+    {
+        private readonly SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+        {
+            DataSource = ".",
+            InitialCatalog = "AHMTZDotNetCore",
+            UserID = "sa",
+            Password = "sa@123"
+        };
+
+        public void Run()
+        {
+            Read();
+            Create("title1", "author1", "content1");
+            Edit(2);
+            Update(2, "title2", "author2", "content2");
+
+        }
+
+        private void Read()
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = "select * from tbl_blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            connection.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine(dr["Blog_Id"].ToString());
+                Console.WriteLine(dr["Blog_Title"].ToString());
+                Console.WriteLine(dr["Blog_Author"].ToString());
+                Console.WriteLine(dr["Blog_Content"].ToString());
+            }
+        }
+
+        private void Edit(int id)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = "select * from tbl_blog where Blog_Id=@Blog_Id";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Blog_Id", id);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+
+            DataRow dr = dt.Rows[0];
+            Console.WriteLine(dr["Blog_Id"].ToString());
+            Console.WriteLine(dr["Blog_Title"].ToString());
+            Console.WriteLine(dr["Blog_Author"].ToString());
+            Console.WriteLine(dr["Blog_Content"].ToString());
+        }
+
+        private void Create(string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([Blog_Title]
+           ,[Blog_Author]
+           ,[Blog_Content])
+     VALUES
+           (@Blog_Title
+           ,@Blog_Author
+           ,@Blog_Content)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Blog_Title", title);
+            cmd.Parameters.AddWithValue("@Blog_Author", author);
+            cmd.Parameters.AddWithValue("@Blog_Content", content);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+            string message = result > 0 ? "Saving Successful.!" : "Saving Failed.";
+            Console.WriteLine(message);
+        }
+
+        private void Update(int id, string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+   SET [Blog_Title] = @Blog_Title
+      ,[Blog_Author] = @Blog_Author
+      ,[Blog_Content] = @Blog_Content
+ WHERE Blog_Id = @Blog_Id";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Blog_Id", id);
+            cmd.Parameters.AddWithValue("@Blog_Title", title);
+            cmd.Parameters.AddWithValue("@Blog_Author", author);
+            cmd.Parameters.AddWithValue("@Blog_Content", content);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+            string message = result > 0 ? "Updating Successful.!" : "Updating Failed.";
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ HPADotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HPADotNetCore.ConsoleApp.AdoDotNetExamples;
+
+namespace HPADotNetCore.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+            adoDotNetExample.Run();
+        }
+    }
+}
