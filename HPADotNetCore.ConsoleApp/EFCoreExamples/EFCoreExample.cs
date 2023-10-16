4db4455e07642a288a16f7537014fba4489bb6b3
Hsu Pyae Aung <hsupyaeaung11219@gmail.com>
2023-10-16 10:07:52 +0630

    [Complete] EF core create,edit and update

--- HPADotNetCore.ConsoleApp/EFCoreExamples/EFCoreExample.cs
+++ HPADotNetCore.ConsoleApp/EFCoreExamples/EFCoreExample.cs
@@ -10,7 +10,10 @@ namespace HPADotNetCore.ConsoleApp.EFCoreExamples
     {
         public void Run()
         {
-            Read();
+            //Read();
+           // Create("EF Core title", "EF Core author", "EF Core content");
+            //Edit(21);
+            Update(21);
         }
         private void Read()
         {
@@ -24,6 +27,49 @@ namespace HPADotNetCore.ConsoleApp.EFCoreExamples
                 Console.WriteLine(item.Blog_Content);
 
             }
+        }
+        private void Edit(int id)
+        {
+            AppDbContext db = new AppDbContext();
+            var item = db.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item == null)
+            {
+                Console.WriteLine("There is no data here!");
+            }
+            Console.WriteLine(item.Blog_Id);
+            Console.WriteLine(item.Blog_Title);
+            Console.WriteLine(item.Blog_Author);
+            Console.WriteLine(item.Blog_Content);
+        }
+        private void Create(string title,string author,string content)
+        {
+            AppDbContext db = new AppDbContext();
+            BlogDataModel model = new BlogDataModel()
+            {
+                Blog_Title = title,
+                Blog_Author = author,
+                Blog_Content = content
+            };
+            db.Blogs.Add(model);
+            int result = db.SaveChanges();
+            var message = result > 0 ? "Saving Successful!" : "Saving Failed.";
+            Console.WriteLine(message);
+        }
+
+        private void Update(int id)
+        {
+            AppDbContext db = new AppDbContext();
+            var item = db.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item == null)
+            {
+                Console.WriteLine("There is no data here.");
+            }
+            item.Blog_Title = "new title1";
+            item.Blog_Author = "new author1";
+            item.Blog_Content = "new content1";
+            int result = db.SaveChanges();
+            var message = result > 0 ? "Update Successful." : "Update Failed.";
+            Console.WriteLine(message);
         }
     }
 }
