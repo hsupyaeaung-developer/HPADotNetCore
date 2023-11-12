875306ab5272341f8bf2bfc4f4899028aed8cc3e
Hsu Pyae Aung <hsupyaeaung11219@gmail.com>
2023-11-13 01:59:24 +0630

    [Complete] DotNet Windows Forms Save

---
+++ HPADotNetCore.WindowsFormsApp/FrmBlog.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace HPADotNetCore.WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(84, 136);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(117, 39);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click_1);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(84, 98);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(100, 26);
+            this.txtContent.TabIndex = 1;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(84, 66);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(100, 26);
+            this.txtAuthor.TabIndex = 2;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(84, 34);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 26);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(497, 436);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.btnSave);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtTitle;
+    }
+}
+
---
+++ HPADotNetCore.WindowsFormsApp/FrmBlog.cs
@@ -0,0 +1,79 @@
+﻿using Dapper;
+using HPADotNetCore.WindowsFormsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HPADotNetCore.WindowsFormsApp
+{
+   
+    public partial class Form1 : Form
+    {
+        private readonly AppDbContext _context;
+        private readonly SqlConnection _sqlConnection;
+        public Form1()
+        {
+            InitializeComponent();
+            AppConfigService appConfigService = new AppConfigService();
+            _context = new AppDbContext(appConfigService.GetDbConnection());
+            _sqlConnection = new SqlConnection(appConfigService.GetDbConnection());
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click_1(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Hello world.");
+            BlogDataModel blog = new BlogDataModel
+            {
+                Blog_Author = txtAuthor.Text,
+                Blog_Content = txtContent.Text,
+                Blog_Title = txtTitle.Text
+            };
+
+            #region EF
+
+            //_context.Blogs.Add(blog);
+            //var result = _context.SaveChanges();
+            //string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+            //MessageBox.Show(message, "Alert", MessageBoxButtons.OK, result > 0 ? MessageBoxIcon.Information : MessageBoxIcon.Error);
+
+            #endregion
+
+            #region Dapper
+
+            string query = $@"INSERT INTO [dbo].[Tbl_Blog]
+           ([Blog_Title]
+           ,[Blog_Author]
+           ,[Blog_Content])
+     VALUES
+           (@Blog_Title
+           ,@Blog_Author
+           ,@Blog_Content)";
+            using (IDbConnection db = new SqlConnection(_sqlConnection.ConnectionString))
+            {
+                var result = db.Execute(query, blog);
+                string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+                MessageBox.Show(message, "Alert", MessageBoxButtons.OK, result > 0 ? MessageBoxIcon.Information : MessageBoxIcon.Error);
+            }
+
+            #endregion
+
+            txtAuthor.Clear();
+            txtContent.Clear();
+            txtTitle.Clear();
+            txtTitle.Focus();
+        }
+    }
+}
