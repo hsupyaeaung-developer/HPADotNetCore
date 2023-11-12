c1cc43f828a38f07286093c5dae4e9087633f2b5
Hsu Pyae Aung <hsupyaeaung11219@gmail.com>
2023-11-13 00:08:18 +0630

    [Complete] Blog List, Create, Update, Delete

---
+++ HPADotNetCore.MvcApp/EFDbContext/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HPADotNetCore.MvcApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace HPADotNetCore.MvcApp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
