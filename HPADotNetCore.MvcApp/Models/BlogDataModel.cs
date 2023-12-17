c667bc4e4a181b10eeb36784ae96b74d987be0f3
Hsu Pyae Aung <hsupyaeaung11219@gmail.com>
2023-12-17 21:11:21 +0630

    [Complete] Blog CRUD with Ajax

--- HPADotNetCore.MvcApp/Models/BlogDataModel.cs
+++ HPADotNetCore.MvcApp/Models/BlogDataModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace HPADotNetCore.MvcApp.Models
 {
@@ -9,10 +12,48 @@ namespace HPADotNetCore.MvcApp.Models
     public class BlogDataModel
     {
         [Key]
-        //[Column("Blog_Id")]
+        [Column("Blog_Id")]
         public int Blog_Id { get; set; }
         public string Blog_Title { get; set; }
         public string Blog_Author { get; set; }
         public string Blog_Content { get; set; }
+    }
+
+    public class BlogDataResponseModel
+    {
+        public PageSettingModel PageSetting { get; set; }
+        public List<BlogDataModel> Blogs { get; set; }
+    }
+
+    public class PageSettingModel
+    {
+        public PageSettingModel()
+        {
+        }
+
+        public PageSettingModel(int pageNo, int pageSize, int pageCount, string pageUrl)
+        {
+            PageNo = pageNo;
+            PageSize = pageSize;
+            PageCount = pageCount;
+            PageUrl = pageUrl;
+        }
+
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+        public string PageUrl { get; set; }
+    }
+
+    public class MessageModel
+    {
+        public MessageModel() { }
+        public MessageModel(bool isSuccess, string message)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
     }
 }
