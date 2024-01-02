86ba2843489701f62584655f00c5b4c92b60a2fe
Hsu Pyae Aung <hsupyaeaung11219@gmail.com>
2024-01-02 11:32:30 +0630

    [Complete] Theme App

---
+++ HPADotNetCore.ThemeMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HPADotNetCore.ThemeMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
