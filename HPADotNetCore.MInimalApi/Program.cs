a81974228223ea5f2c640ebb36f96df16523ad2f
Hsu Pyae Aung <hsupyaeaung11219@gmail.com>
2024-01-13 21:34:50 +0630

    [Complete] Minimal Api Read, Create

--- HPADotNetCore.MInimalApi/Program.cs
+++ HPADotNetCore.MInimalApi/Program.cs
@@ -1,34 +1,47 @@
+using HPADotNetCore.MinimalApi;
+using HPADotNetCore.MinimalApi.Features.Blog;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.AddControllersWithViews().AddJsonOptions(opt =>
+//{
+//    opt.JsonSerializerOptions.PropertyNameCaseInsensitive = false;
+//    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+//});
+
+builder.Services.ConfigureHttpJsonOptions(option =>
+{
+    option.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    option.SerializerOptions.PropertyNamingPolicy = null;
+});
+
+
 // Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+},
+ServiceLifetime.Transient,
+ServiceLifetime.Transient);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.AddBlogService();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ HPADotNetCore.MinimalApi/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HPADotNetCore.MinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPADotNetCore.MinimalApi
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
