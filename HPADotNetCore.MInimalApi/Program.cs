1f7d2f4884df0efab80f417a3b70757a051e7627
Hsu Pyae Aung <hsupyaeaung11219@gmail.com>
2024-01-21 00:31:09 +0630

    [Complete] Serilog TextLog and DbLog in MinimaleApi

--- HPADotNetCore.MInimalApi/Program.cs
+++ HPADotNetCore.MInimalApi/Program.cs
@@ -1,46 +1,74 @@
-using HPADotNetCore.MinimalApi;
-using HPADotNetCore.MinimalApi.Features.Blog;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json.Serialization;
-var builder = WebApplication.CreateBuilder(args);
+    using HPADotNetCore.MinimalApi;
+    using HPADotNetCore.MinimalApi.Features.Blog;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text.Json.Serialization;
+    using Newtonsoft.Json.Serialization;
+    using Serilog;
+    using Serilog.Sinks.MSSqlServer;
+    using System.Reflection;
 
-//builder.Services.AddControllersWithViews().AddJsonOptions(opt =>
-//{
-//    opt.JsonSerializerOptions.PropertyNameCaseInsensitive = false;
-//    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-//});
+//get current project name
+var projectName = Assembly.GetCallingAssembly().GetName().Name;
 
-builder.Services.ConfigureHttpJsonOptions(option =>
-{
-    option.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-    option.SerializerOptions.PropertyNamingPolicy = null;
-});
+Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.File($"logs/{projectName}.txt", rollingInterval: RollingInterval.Hour, fileSizeLimitBytes: 1024 * 100)
+        .WriteTo
+        .MSSqlServer(
+        connectionString: "Server=DESKTOP-HDNAOSB\\SQL2022;Database=TestDb;User ID =sa;Password=sa@123;TrustServerCertificate=true;",
+        sinkOptions: new MSSqlServerSinkOptions { TableName = "MinimalApiLogEvents", AutoCreateSqlTable = true })
+        .CreateLogger();
+try
+    {
+        Log.Information("Starting web application");
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Host.UseSerilog();
+        //builder.Services.AddControllersWithViews().AddJsonOptions(opt =>
+        //{
+        //    opt.JsonSerializerOptions.PropertyNameCaseInsensitive = false;
+        //    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        //});
 
-builder.Services.AddDbContext<AppDbContext>(opt =>
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-},
-ServiceLifetime.Transient,
-ServiceLifetime.Transient);
+        builder.Services.ConfigureHttpJsonOptions(option =>
+    {
+        option.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        option.SerializerOptions.PropertyNamingPolicy = null;
+    });
 
-var app = builder.Build();
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+    {
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+    },
+    ServiceLifetime.Transient,
+    ServiceLifetime.Transient);
 
-app.UseHttpsRedirection();
+    var app = builder.Build();
 
-app.AddBlogService();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.Run();
+    app.UseHttpsRedirection();
+
+    app.AddBlogService();
+
+    app.Run();
+    }
+    catch (Exception ex)
+    {
+        Log.Fatal(ex, "Application terminated unexpectedly");
+    }
+    finally
+    {
+        Log.CloseAndFlush();
+    }
