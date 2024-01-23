aacd4869bc2eeb7bfde00a8a287240db1e14a4c5
Hsu Pyae Aung <hsupyaeaung11219@gmail.com>
2024-01-23 19:39:32 +0630

    [Complete] NLog text logging

---
+++ HPADotNetCore.NLoggingMinimalApi/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HPADotNetCore.NLoggingMinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPADotNetCore.NLoggingMinimalApi
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
