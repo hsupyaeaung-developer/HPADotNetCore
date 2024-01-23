b68c090de4392911754396a1780d07b77af86f2f
Hsu Pyae Aung <hsupyaeaung11219@gmail.com>
2024-01-23 20:13:43 +0630

    [Complete] Log4net text logging

---
+++ HPADotNetCore.Log4NetLoggingMinimalApi/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HPADotNetCore.Log4NetLoggingMinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPADotNetCore.Log4NetLoggingMinimalApi
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
